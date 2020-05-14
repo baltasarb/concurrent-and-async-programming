19cea2e1c2409a9c9ecf8fd00d0ae23f75d804dd
baltasarb <baltasar.brito@gmail.com>
2020-05-14 18:11:18 +0100

    feat: Add crosscutting project with monitor and timer utilities

---
+++ dotnet/ConcurrentAndAsyncProgramming/CrossCutting/MonitorUtilities.cs
@@ -0,0 +1,41 @@
+﻿namespace CrossCutting
+{
+    using System.Threading;
+
+    public static class MonitorUtilities
+    {
+        public static void EnterUninterruptedly(object monitor, out bool interrupted)
+        {
+            var lockTaken = interrupted = false;
+
+            while (!lockTaken)
+            {
+                try
+                {
+                    Monitor.Enter(monitor, ref lockTaken);
+                }
+                catch (ThreadInterruptedException)
+                {
+                    interrupted = true;
+                }
+            }
+        }
+
+        private static void LockImp(object monitor)
+        {
+            bool lockTaken = false;
+
+            try
+            {
+                Monitor.Enter(monitor, ref lockTaken);
+            }
+            finally
+            {
+                if (lockTaken)
+                {
+                    Monitor.Exit(monitor);
+                }
+            }
+        }
+    }
+}
---
+++ dotnet/ConcurrentAndAsyncProgramming/CrossCutting/Timer.cs
@@ -0,0 +1,27 @@
+﻿namespace CrossCutting
+{
+    using System;
+
+    public class Timer
+    {
+        private readonly DateTime expirationTime;
+
+        public Timer(int duration)
+        {
+            this.expirationTime = DateTime.UtcNow.AddMilliseconds(duration);
+        }
+
+        public bool IsExpired()
+        {
+            var currentTime = DateTime.UtcNow;
+
+            return currentTime.CompareTo(expirationTime) >= 0;
+        }
+
+        public int GetTimeToWait()
+        {
+            var timeSpan = expirationTime - DateTime.UtcNow;
+            return (int)Math.Floor(timeSpan.TotalMilliseconds);
+        }
+    }
+}
