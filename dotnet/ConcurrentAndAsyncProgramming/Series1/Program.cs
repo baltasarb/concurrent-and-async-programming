5b674ab12ab45811d22509d63d17be194da72a41
baltasarb <baltasar.brito@gmail.com>
2020-05-14 18:10:19 +0100

    feat: Add project with series 1

---
+++ dotnet/ConcurrentAndAsyncProgramming/Series1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Series1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
