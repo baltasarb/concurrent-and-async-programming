6dbe701ae05c043e88adf119034a3c31d04f0d62
baltasarb <baltasar.brito@gmail.com>
2020-05-14 18:12:14 +0100

    feat: Add 3 transfer queue solution

---
+++ dotnet/ConcurrentAndAsyncProgramming/Series1/3 TransferQueue/Message.cs
@@ -0,0 +1,24 @@
+﻿namespace Series1._3_TransferQueue
+{
+    using CrossCutting;
+    using System.Threading;
+
+    class Message<E>
+    {
+        public readonly object condition;
+
+        public E body;
+
+        public Message(E body = default, object condition = default)
+        {
+            this.body = body;
+            this.condition = condition;
+        }
+        public void NotifyWaiter()
+        {
+            MonitorUtilities.EnterUninterruptedly(condition, out _);
+            Monitor.Pulse(condition);
+            Monitor.Exit(condition);
+        }
+    }
+}
---
+++ dotnet/ConcurrentAndAsyncProgramming/Series1/3 TransferQueue/TransferQueue.cs
@@ -0,0 +1,141 @@
+﻿namespace Series1._3_TransferQueue
+{
+    using CrossCutting;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+
+    class TransferQueue<E> where E : class
+    {
+        private readonly object monitor;
+        private readonly IList<Message<E>> consumers;
+        private readonly IList<Message<E>> producers;
+
+        public TransferQueue()
+        {
+            this.monitor = new object();
+            this.consumers = new List<Message<E>>();
+            this.producers = new List<Message<E>>();
+        }
+        public void Put(E body)
+        {
+            MonitorUtilities.EnterUninterruptedly(this.monitor, out _);
+            var message = new Message<E>(body);
+            producers.Add(message); ;
+            Monitor.Exit(this.monitor);
+        }
+
+        public bool Transfer(E body, int timeout)
+        {
+            Message<E> message = default;
+
+            try
+            {
+                MonitorUtilities.EnterUninterruptedly(monitor, out _);
+
+                message = this.consumers.First();
+
+                if (message != default)
+                {
+                    this.consumers.Remove(message);
+                    Monitor.Exit(this.monitor);
+                    message.body = body;
+                    message.NotifyWaiter();
+                    return true;
+                }
+
+                message = new Message<E>(body, condition: new object());
+                this.producers.Add(message);
+
+                var timer = new CrossCutting.Timer(timeout);
+
+                while (true)
+                {
+                    Monitor.Wait(timeout);
+
+                    if (message.body != default)
+                    {
+                        Monitor.Exit(this.monitor);
+                        return true;
+                    }
+
+                    if (timer.IsExpired())
+                    {
+                        this.producers.Remove(message);
+                        Monitor.Exit(this.monitor);
+                        return false;
+                    }
+                }
+            }
+            catch (ThreadInterruptedException)
+            {
+                if (message?.body != default)
+                {
+                    Monitor.Exit(this.monitor);
+                    Thread.CurrentThread.Interrupt();
+                    return true;
+                }
+                Monitor.Exit(this.monitor);
+                throw;
+            }
+        }
+
+        public E Take(int timeout)
+        {
+            Message<E> message = default;
+            try
+            {
+                MonitorUtilities.EnterUninterruptedly(monitor, out _);
+
+                message = this.producers.First();
+
+                if (message != default)
+                {
+                    this.producers.Remove(message);
+                    Monitor.Exit(this.monitor);
+
+                    //condition might be default if it was added to the producers queue by Put()
+                    if (message.condition != default)
+                    {
+                        message.NotifyWaiter();
+                    }
+                    return message.body;
+                }
+
+                message = new Message<E>(default, condition: new object());
+                this.consumers.Add(message);
+
+                var timer = new CrossCutting.Timer(timeout);
+
+                while (true)
+                {
+                    Monitor.Wait(timeout);
+
+                    if (message.body != default)
+                    {
+                        Monitor.Exit(this.monitor);
+                        return message.body;
+                    }
+
+                    if (timer.IsExpired())
+                    {
+                        this.consumers.Remove(message);
+                        Monitor.Exit(this.monitor);
+                        return null;
+                    }
+                }
+            }
+            catch (ThreadInterruptedException)
+            {
+                if (message?.body != default)
+                {
+                    Monitor.Exit(this.monitor);
+                    Thread.CurrentThread.Interrupt();
+                    return message.body;
+                }
+                Monitor.Exit(this.monitor);
+                throw;
+            }
+        }
+    }
+}
